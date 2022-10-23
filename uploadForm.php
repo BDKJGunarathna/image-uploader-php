5033b578f10209d3c379a49693ef9e4e7a790366
BDKJGunarathna <kasungunarathna814@gmail.com>
2022-10-23 22:06:34 +0530

    javascript functions added

--- uploadForm.php
+++ uploadForm.php
@@ -9,6 +9,7 @@
 <body>
     <div class="main-parent">
         <div class="main-container">
+
             <form action="" method="post">
                 <h1 class="title">Add </h1>
                 <div class="img">
@@ -22,18 +23,39 @@
 
                 </div>
                 <div class="addImg">
-                    <input type="image" name="addImage" class="img-upload" style="display: none;">
+                    <input type="files" name="addImage" onchange="displayImage(this)" class="img-upload" style="display: none;">
                     <label>Image</label>
                 </div>
+
                 <div class="img-caption">
                     <label>Add Caption</label>
                     <textarea name="caption"></textarea>
                 </div>
+
                 <div class="img-svBtn">
                     <button type="submit" name="save" >Save</button>
                 </div>
+
+
             </form>
+
         </div>
     </div>
 </body>
 </html>
+
+<script type="text/javascript" >
+    function addImage(e){
+        document.querySelector('#img-upload').click();
+    }
+
+    function displayImage(e){
+        if(e.files[0]){
+            var fileRead=new FileReader();
+            fileRead.onload=function(e){
+                document.querySelector('#profileDisplay').setAttribute('src',e.target.result);
+            }
+            fileRead.readAsDataURL(e.files[0]);
+        }
+    }
+</script>
