aacb37d6004f057a2a89556cd7c2b3433a959ec3
BDKJGunarathna <kasungunarathna814@gmail.com>
2022-10-23 20:32:42 +0530

    form added

--- uploadForm.php
+++ uploadForm.php
@@ -1,4 +1,24 @@
-<?php
+<?php ?>
 
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<title>Image-Uploader</title>
+<body>
+    <div class="main-parent">
+        <div class="main-container">
+            <form action="" method="post">
+                <h1 class="title">Add </h1>
+                <div class="img">
+                    <div class="img-plcehlder" onclick="addImage()">
+                        <h3>Upload Image</h3>
+                    </div>
+                    <img src=""
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
